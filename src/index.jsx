abceed4818da3528d367fc78df3c6f025cd9ecaf
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-08-23 18:09:40 -0400

    index.jsx setup with initial state and reducers created

--- src/index.jsx
+++ src/index.jsx
@@ -8,9 +8,27 @@ import { createStore, combineReducers } from 'redux';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+// importing reducers
+import messagesReducer from './reducers/messages_reducer';
+import channelsReducer from './reducers/channels_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
+import currentUsernameReducer from './reducers/current_username_reducer';
+
+// Initial state
+const initialState = {
+  messages: [],
+  channels: ['general', 'react', 'paris'],
+  selectedChannel: 'general',
+  currentUsername: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
+};
+
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  // changeMe: (state = null, action) => state
+  messages: messagesReducer,
+  channels: channelsReducer,
+  selectedChannel: selectedChannelReducer,
+  currentUsername: currentUsernameReducer
 });
 
 // render an instance of the component in the DOM
